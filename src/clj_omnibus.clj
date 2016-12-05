19da4c553b5160d4d37c28a49548ee4578f0bf58
Marcus <jaybuffay@aol.com>
2016-12-05 16:37:10 +0900

    test

--- src/clj_omnibus.clj
+++ src/clj_omnibus.clj
@@ -6,11 +6,15 @@ Available options are:
 1. atoms		: Lists all atoms used by clojure
 2. structs		: Lists all data structures used by clojure
 3. funcs		: Lists all function types and parameters used by clojure
--. (define [option])	: Outputs details of an above option in human-readable format.
--. (help)				: Displays this help message.\n"))
+-. (define [option])	: Elaborates an above option in human-readable format.
+-. (help)		: Displays this help message.
+-. (rr)			: Reloads the namespace.\n"))
 
 (defn define [data]
 	(print data))
+
+(defn rr [] "This function reloads the namespace"
+	(use 'clj-omnibus :reload-all))
 
 ;========================~ Atoms ~===============================;
 (def atoms '(true false nil))
